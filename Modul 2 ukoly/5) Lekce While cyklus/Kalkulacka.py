d107e13733257572e7551bca77accf606e9aa9c5
bara125 <bara1pokorna@gmail.com>
2025-02-11 13:04:45 +0100

    kalkulacka

---
+++ Modul 2 ukoly/5) Lekce While cyklus/Kalkulacka.py
@@ -0,0 +1,29 @@
+separator = "-" * 40
+print("Scitani: '+', ",
+      "Odcitani: '-', ",
+      sep= "\n")
+print(separator)
+while True:
+    operace = input("Vyber si operaci:")
+    if operace != "+" and operace != "-":
+        print("Nezadali jste platny operator, zkuste to znovu")
+        print(separator)
+        continue
+    else:
+        cislo_1 = int(input("Zadej prvni cislo:"))
+        cislo_2 = int(input("Zadej druhe cislo:"))
+        if operace == "+":
+                vysledek = cislo_1 + cislo_2
+        elif operace == "-":
+                vysledek = cislo_1 - cislo_2
+        print(cislo_1, operace, cislo_2, " = ", vysledek)
+        pokracovani = input("Chcete provést další operaci?('a' pro ano, jakákoliv jiná klávesa pro ne):")
+        if pokracovani != "a":
+              print("Ukoncuji kalkulacku")
+              break
+
+# Dle operátoru proveď výpočet a vypiš ho - verze s f stringem
+    #if operation == '+':
+       # print(f'{number_1} + {number_2} = {number_1 + number_2}')
+    #elif operation == '-':
+        #print(f'{number_1} - {number_2} = {number_1 - number_2}')
